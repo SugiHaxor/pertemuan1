8cf8b56e8d587897db3ea72fd0912531f3c47519
SugiHaxor <67062224+SugiHaxor@users.noreply.github.com>
2020-06-17 21:56:04 +0700

    Add files via upload

---
+++ pertemuan2/GanjilGenap.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pertemuan2;
+
+/**
+ *
+ * @author sugi
+ */
+public class GanjilGenap {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int bilangan = 10;
+    
+        if(bilangan%2 == 0){
+            System.out.println("Bilangan Genap");
+        }else{
+            System.out.println("Bilangan Ganjil");
+        
+    }   
+}
+}
